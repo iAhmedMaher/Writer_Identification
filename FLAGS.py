57cbb9da68b0b67aba014ba2dc340c370d0e5b95
Ahmed Maher AlAmir <iahmedmaher@gmail.com>
2018-12-02 11:08:18 +0200

    minor changes

--- FLAGS.py
+++ FLAGS.py
@@ -1,2 +1,2 @@
-TWO_FORM_DATASET_PATH = r'E:\handwritten_mini_dataset'
+TWO_FORM_DATASET_PATH = r'E:\handwritten_dataset'
 WRITER_IMG_PAIR_INFO_PATH = r'D:\Projects\Pattern_Recognition\Writer_Identification\forms.txt'
