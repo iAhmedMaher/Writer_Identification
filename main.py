421496264aff2527ac94ac48e1d56a53b353da30
Ahmed Salah <ahmedsalahcie@gmail.com>
2018-12-02 14:18:47 +0200

    Fixed main.py
    - Random testing
    - Classification Naming

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import random
 import skimage.io as io
 from os import listdir
 from os.path import isfile, join
+import numpy as np
 
 testWriterFileName=None
 X_Train=[]
@@ -13,16 +14,37 @@ X_Test=[]
 Y_Train=[]
 Y_Test=[]
 
+def processImages(dataPath):
+	X = None
+	Y = []
+	fileNames = listdir(dataPath)
+	print(fileNames)
+	for f in fileNames:
+		test_img = io.imread(join(dataPath, f))
+		textureBlocks = Preprocessing(test_img)
+		for textureBlock in textureBlocks:
+			if X is not None:
+				X = np.concatenate([X, getFeatureVector(textureBlock)] , axis=0)
+			else:
+				X = getFeatureVector(textureBlock)
+			print("*****************************")
+			Y.append(f.split('_')[0])
+	print(Y)
+	return X, np.array(Y)
+
 def processTestImages(dataPath):
 	global testWriterFileName
-	X=[]
+	X=None
 	Y=[]
 	test_img = io.imread(join(dataPath, testWriterFileName))
 	textureBlocks = Preprocessing(test_img)
-	# for textureBlock in textureBlocks:
-	X.append(getFeatureVector(textureBlocks, 256))
-	Y.append(str(testWriterFileName).split('_')[0])
-	return X,Y
+	for textureBlock in textureBlocks:
+		if X is not None:
+			X = np.concatenate([X, getFeatureVector(textureBlock)], axis=0)
+		else:
+			X = getFeatureVector(textureBlock)
+		Y.append(str(testWriterFileName).split('_')[0])
+	return X,np.array(Y)
 
 def processTrainingImages(dataPath, numWriters):
 	global testWriterFileName
@@ -31,50 +53,60 @@ def processTrainingImages(dataPath, numWriters):
 	availableWriters=list(range(numWriters))
 	rndmWriters = []
 	while(len(rndmWriters)<3):
-		rndmWriter = random.randint(0, len(availableWriters))
+		rndmWriter = random.randint(0, len(availableWriters)-1)
 		#Chosen writer must have at least 2 forms
-		writerForms = [f for f in fileNames if f.startswith(availableWriters[rndmWriter])]
+		writerForms = [f for f in fileNames if f.startswith(str(availableWriters[rndmWriter]))]
 		if (len(writerForms) <= 1 and len(rndmWriters)<2) or (len(writerForms)<=2 and len(rndmWriters)==2):
 			availableWriters.pop(rndmWriter)
 			continue
 		rndmWriters.append(availableWriters[rndmWriter])
 		availableWriters.pop(rndmWriter)
 
-	X=[]
+	X=None
 	Y=[]
 	for i in range(3):
-		writerForms=[f for f in fileNames if f.startswith(i)]
-		availableForms = list(range(writerForms))
+		writerForms=[f for f in fileNames if f.startswith(str(rndmWriters[i]))]
+		availableForms = list(range(len(writerForms)))
 
-		#Process first writer form
-		for i in range(2):
-			rndmForm = random.randint(0,len(availableForms))
+		for j in range(2):
+			rndmForm = random.randint(0,len(availableForms)-1)
 			form = writerForms[availableForms[rndmForm]]
 			training_img = io.imread(join(dataPath, form))
 			textureBlocks = Preprocessing(training_img)
-			#for textureBlock in textureBlocks:
-			X.append(getFeatureVector(textureBlocks, 256))
-			Y.append(i)
+			for textureBlock in textureBlocks:
+				if X is not None:
+					X = np.concatenate([X, getFeatureVector(textureBlock)], axis=0)
+				else:
+					X = getFeatureVector(textureBlock)
+				Y.append(form.split("_")[0])
 			availableForms.pop()
 
 		if i==2:
 			rndmForm = random.randint(0, len(availableForms))
 			testWriterFileName = writerForms[availableForms[rndmForm]]
 
-	return X,Y
+	return X,np.array(Y)
 
 """Main Function"""
 if __name__ == '__main__':
 	trainingDataPath = "Training"
 	testDataPath = "Test"
+	rndmDataPath = "handwritten_dataset"
 
-	X_Train,Y_Train = processImages(trainingDataPath)
-	X_Test, Y_Test = processImages(testDataPath)
+	print("Getting Images")
 
-	trainingFiles = [f for f in listdir(trainingDataPath)]
-	adaboost_clf(Y_Train, X_Train, numClassifiers=50, learnRate=1.5, clfNum=3)
-	testFiles = [f for f in listdir(testDataPath)]
+	#X_Train,Y_Train = processImages(trainingDataPath)
+	#X_Test, Y_Test = processImages(testDataPath)
+
+	X_Train, Y_Train = processTrainingImages(rndmDataPath, 671)
+	X_Test, Y_Test = processTestImages(rndmDataPath)
+
+	print("# X training:", len(X_Train))
+
+	adaboost_clf(Y_Train, X_Train, numClassifiers=50, learnRate=1.5, clfNum=2)
+
+	print("Boosted")
+
 	predict_clf(Y_Test,X_Test)
-
 
 
