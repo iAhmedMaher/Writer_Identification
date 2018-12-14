d40ec4abadc4a952ed511930f1511eaabfa91ef0
Ahmed Maher AlAmir <iahmedmaher@gmail.com>
2018-12-14 12:56:06 +0200

    Merge branch 'master' of https://github.com/iAhmedMaher/Writer_Identification
    # Conflicts:
    #	Preprocessing.py

--- FLAGS.py
+++ FLAGS.py
@@ -1,7 +1,8 @@
 TWO_FORM_DATASET_PATH = r'E:\handwritten_dataset'
 TWO_FORM_MINI_DATASET_PATH = r'E:\handwritten_mini_dataset'
+RELATIVE_PATH = ''
 WRITER_IMG_PAIR_INFO_PATH = r'D:\Projects\Pattern_Recognition\Writer_Identification\forms.txt'
-DEFAULT_DATASET_PATH = TWO_FORM_DATASET_PATH
+DEFAULT_DATASET_PATH = RELATIVE_PATH
 CACHE_BLOCKS = False
 TEXTURE_BLOCKS_LOG_PATH = r'texture_blocks.txt'
 CACHE_FEATURE_VECTORS = {
